fb0e42d43df9c879b31a4bc9326dccc8a08d3927
togauthi <togauthier23@gmail.com>
2025-04-29 13:46:39 +0200

    Better error message

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -10,27 +10,27 @@ Harl::Harl() {
 
 void Harl::debug()
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "[DEBUG]" << std::endl << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Harl::warning()
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "[WARNING]" << std::endl << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void Harl::error()
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ERROR]" << std::endl << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::info()
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" <<std::endl;
+	std::cout << "[INFO]" << std::endl << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" <<std::endl;
 }
 
 void Harl::complain(std::string level)
 {
-	std::string lvls[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	std::string lvls[5] = { "DEBUG", "INFO", "WARNING", "ERROR" };
 	for (int i = 0; i < 4; i++)
 		if (lvls[i] == level)
 			(this->*harl[i])();
